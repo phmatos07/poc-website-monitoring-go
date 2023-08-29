ea7ca448e799190fa93ffeb2a1d84df3b91ca80c
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2023-08-29 15:52:13 -0300

    feat: Criação e implementação do módulo monitoring.

--- src/main.go
+++ src/main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/phmatos07/poc-website-monitoring-go/command"
+	"github.com/phmatos07/poc-website-monitoring-go/monitoring"
 )
 
 func main() {
-	var data = command.Get()
-	fmt.Println(data)
+
+	_command := command.Get()
+
+	if _command == command.TypesCommands[1] {
+		monitoring.ToMonitor()
+		return
+	}
+
+	if _command == command.TypesCommands[2] {
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	}
 }
